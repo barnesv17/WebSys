4e823ec4678e58ed50abf7cd76fc857c31d7218d
Kyle Qin <kylezehaoqin@users.noreply.github.com>
2020-10-06 19:25:56 -0400

    Added css Added id name for divs Finished part 3

--- lab4/lab4.js
+++ lab4/lab4.js
@@ -12,6 +12,28 @@ function getElementsRecursively(element, level, ret) {
     return ret;
 }
 
+// Part3
+function addQuote() {
+    var temp = document.getElementsByTagName("div")[3];
+    var parent = temp.parentElement;
+    var clone = temp.cloneNode();
+    clone.innerHTML = "<h2>Quote</h2>";
+    clone.innerHTML += "Any application that can be written in JavaScript, will eventually be written in JavaScript.<br><strong>Jeff Atwood, 2007</strong>";
+    parent.appendChild(clone);
+}
+function styleManipulation() {
+    var divs = document.getElementsByTagName("div");
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("mouseover", function() {
+            this.style.backgroundImage = "linear-gradient(225deg, #7A88FF, #A0D2EB, #98ffee, #7AFFAF)"
+            this.style.paddingLeft = "40px";
+        }, false);
+        divs[i].addEventListener("mouseout", function() {
+            this.style.backgroundImage = "";
+            this.style.paddingLeft = "30px";
+        }, false);
+    }
+}
 
 // =================================== Main =========================================
 
@@ -21,4 +43,6 @@ function getElementsRecursively(element, level, ret) {
 window.onload = function() {
     document.getElementById("info").innerHTML = 
         getElementsRecursively(document.getElementsByTagName("html")[0], "", "");
+window.onload = addQuote();
+window.onload = styleManipulation();
 }
