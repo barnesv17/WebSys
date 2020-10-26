565872bd14215b5afcd7cce0b013a924cc1e524e
barnesv17 <virginiabarnes0825@gmail.com>
2020-10-26 14:33:45 -0400

    Validates box input now

--- lab5&6/hexed.js
+++ lab5&6/hexed.js
@@ -5,6 +5,8 @@ var blue = Math.floor( Math.random() * 256 );
 var rgb = "rgb(" + red + "," + green + "," + blue + ")";
 box.style.backgroundColor = rgb;
 
+var re = new RegExp( /^0x[0-9A-F]{2}$/ );
+
 var rslider = document.getElementById("Rrange");
 var routput = document.getElementById("Rdemo");
 var rinput = document.getElementById("Rtext");
@@ -22,42 +24,61 @@ var binput = document.getElementById("Btext");
 boutput.innerHTML = "0x" + Number(bslider.value).toString(16).toUpperCase(); // Display the default slider value
 
 
-// Update the current slider value (each time you drag the slider handle)
-rslider.oninput = function() {
+//---- RED ---------------------------------------------------------------------
+// TO DO: HANDLE INVALID BOX INPUT
+rslider.oninput = function() { // If slider is changed...
   var hexString = "0x" + Number(this.value).toString(16).toUpperCase()
   routput.innerHTML = hexString;
   rinput.value = hexString;
 }
-rinput.onkeypress = function( e ) {
+rinput.onkeypress = function( e ) { // If textbox is changed...
   if (e.keyCode == '13' ) {
-    var dec = parseInt( this.value, 16 );
-    rslider.value = dec;
-    routput.innerHTML = this.value;
+    // Check for valid box input
+    if ( !this.value.match(re) ) {
+      alert( "Please enter a valid hex value in the format '0x__'" );
+    }
+    else {
+      var dec = parseInt( this.value, 16 );
+      rslider.value = dec;
+      routput.innerHTML = this.value;
+    }
   }
 }
-// Update the current slider value (each time you drag the slider handle)
-gslider.oninput = function() {
+//---- GREEN ---------------------------------------------------------------------
+gslider.oninput = function() { // If slider is changed...
   var hexString = "0x" + Number(this.value).toString(16).toUpperCase()
   goutput.innerHTML = hexString;
   ginput.value = hexString;
 }
-ginput.onkeypress = function( e ) {
+ginput.onkeypress = function( e ) { // If textbox is changed...
   if (e.keyCode == '13' ) {
-    var dec = parseInt( this.value, 16 );
-    gslider.value = dec;
-    goutput.innerHTML = this.value;
+    // Check for valid box input
+    if ( !this.value.match(re) ) {
+      alert( "Please enter a valid hex value in the format '0x__'" );
+    }
+    else {
+      var dec = parseInt( this.value, 16 );
+      gslider.value = dec;
+      goutput.innerHTML = this.value;
+    }
   }
 }
-// Update the current slider value (each time you drag the slider handle)
-bslider.oninput = function() {
+//---- BLUE ---------------------------------------------------------------------
+bslider.oninput = function() { // If slider is changed...
   var hexString = "0x" + Number(this.value).toString(16).toUpperCase()
   boutput.innerHTML = hexString;
   binput.value = hexString;
 }
-binput.onkeypress = function( e ) {
+binput.onkeypress = function( e ) { // If textbox is changed...
   if (e.keyCode == '13' ) {
-    var dec = parseInt( this.value, 16 );
-    bslider.value = dec;
-    boutput.innerHTML = this.value;
+    // Check for valid box input
+    if ( !this.value.match(re) ) {
+      alert( "Please enter a valid hex value in the format '0x__'" );
+    }
+    else {
+      var dec = parseInt( this.value, 16 );
+      bslider.value = dec;
+      boutput.innerHTML = this.value;
+    }
   }
 }
