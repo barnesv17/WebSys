aa9a5838b78fe299d214a7fb87b5792ad873a66f
barnesv17 <virginiabarnes0825@gmail.com>
2020-10-26 15:19:26 -0400

    Added percentage functionality

--- lab5&6/hexed.js
+++ lab5&6/hexed.js
@@ -10,18 +10,21 @@ var re = new RegExp( /^0x[0-9A-F]{2}$/ );
 var rslider = document.getElementById("Rrange");
 var routput = document.getElementById("Rdemo");
 var rinput = document.getElementById("Rtext");
-routput.innerHTML = "0x" + Number(rslider.value).toString(16).toUpperCase(); // Display the default slider value
+routput.innerHTML = "0x" + Number(rslider.value).toString(16).toUpperCase();
 
 var gslider = document.getElementById("Grange");
 var goutput = document.getElementById("Gdemo");
 var ginput = document.getElementById("Gtext");
-goutput.innerHTML = "0x" + Number(gslider.value).toString(16).toUpperCase(); // Display the default slider value
+goutput.innerHTML = "0x" + Number(gslider.value).toString(16).toUpperCase();
 
 
 var bslider = document.getElementById("Brange");
 var boutput = document.getElementById("Bdemo");
 var binput = document.getElementById("Btext");
-boutput.innerHTML = "0x" + Number(bslider.value).toString(16).toUpperCase(); // Display the default slider value
+boutput.innerHTML = "0x" + Number(bslider.value).toString(16).toUpperCase();
+
+var guess = document.getElementById("guessButton");
+var percents = document.getElementById( "percents" );
 
 
 //---- RED ---------------------------------------------------------------------
@@ -44,7 +47,7 @@ rinput.onkeypress = function( e ) { // If textbox is changed...
     }
   }
 }
-//---- GREEN ---------------------------------------------------------------------
+//---- GREEN -------------------------------------------------------------------
 gslider.oninput = function() { // If slider is changed...
   var hexString = "0x" + Number(this.value).toString(16).toUpperCase()
   goutput.innerHTML = hexString;
@@ -63,7 +66,7 @@ ginput.onkeypress = function( e ) { // If textbox is changed...
     }
   }
 }
-//---- BLUE ---------------------------------------------------------------------
+//---- BLUE --------------------------------------------------------------------
 bslider.oninput = function() { // If slider is changed...
   var hexString = "0x" + Number(this.value).toString(16).toUpperCase()
   boutput.innerHTML = hexString;
@@ -81,4 +84,42 @@ binput.onkeypress = function( e ) { // If textbox is changed...
       boutput.innerHTML = this.value;
     }
   }
+}
+//---- GUESS BUTTON ------------------------------------------------------------
+function percentOff( correct, guess ) {
+  return Math.round( Math.abs(( correct - guess )/255 ) * 100 );
+}
+guess.onclick = function() {
+  var roff = percentOff( red, rslider.value );
+  var goff = percentOff( green, gslider.value );
+  var boff = percentOff( blue, bslider.value );
+  var rstring = "<p>Red: ";
+  var gstring = "<p>Green: ";
+  var bstring = "<p>Blue: ";
+  var correctstring = "You got it! (0&#37; off)</p>";
+  var incorrectstring = "&#37; off</p>"
+
+  if ( roff == 0 ) {
+    rstring += correctstring;
+  }
+  else {
+    rstring += roff + incorrectstring;
+  }
+
+  if ( goff == 0 ) {
+    gstring += correctstring;
+  }
+  else {
+    gstring += goff + incorrectstring;
+  }
+
+  if ( boff == 0 ) {
+    bstring += correctstring;
+  }
+  else {
+    bstring += boff + incorrectstring;
+  }
+
+  var output = rstring + gstring + bstring;
+  percents.innerHTML = output;
 }
