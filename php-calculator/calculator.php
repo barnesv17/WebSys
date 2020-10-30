d27f92bc767d84a815e9132f2f2d29e70a0a85b6
kylezehaoqin <qinz@rpi.edu>
2020-10-30 14:25:29 -0400

    nvm

--- php-calculator/calculator.php
+++ php-calculator/calculator.php
@@ -53,11 +53,7 @@ class Multiplication extends Operation {
 class Division extends Operation {
   public function operate() {
     if ($this->operand_2 == 0) {
-      if ($this->operand_1 == 0) {
-        return 1;
-      } else {
-        return "UNDEFINED";
-      }
+      return "UNDEFINED";
     }
 
     return (float)($this->operand_1) / (float)($this->operand_2);
