84c3218c2b1ad8d7525f62b394464333e55c1152
derekli2000 <derekli2011@gmail.com>
2020-11-10 12:56:57 -0500

    added 8-10 from part 2

--- lab9/db.sql
+++ lab9/db.sql
@@ -5,7 +5,7 @@ create table courses (
     prefix varchar(4) not null,
     number smallint(4) not null,
     title varchar(255) not null
-)
+);
 
 -- Create a table named students. It should contain RIN (int 9, primary key), RCSID (char 7), first name (varchar 100, not null), last name (varchar 100, not null), alias (varchar 100, not null), and phone (int 10). Collate should be utf8_general_ci.
 
@@ -16,4 +16,12 @@ create table students (
     lastname varchar(100) not null,
     alias varchar(100) not null,
     phone int(10)
-)
+);
+
+-- 8. List all students RIN, name, and address if their grade in any course was higher than a 90
+-- 9. List out the average grade in each course
+-- 10. List out the number of students in each course
+
+select s.RIN, s.firstname, s.lastname, s.address from students s, grades g where s.RIN = g.RIN and g.grade > 90;
+select avg(grade) from grades group by crn;
+select count(RIN) from grades group by crn;
