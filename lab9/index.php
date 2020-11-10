88957dbdb72e778de80065996f718c11eae1e9c0
barnesv17 <virginiabarnes0825@gmail.com>
2020-11-10 13:15:09 -0500

    connecting to db

--- lab9/index.php
+++ lab9/index.php
@@ -2,9 +2,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$db = "websyslab9";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
