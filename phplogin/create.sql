feb15d85ce02339887edec54cb873fade95bd216
gwild37 <gabewild37@gmail.com>
2020-11-13 13:50:53 -0500

    this doesn't work idk what to do

--- phplogin/create.sql
+++ phplogin/create.sql
@@ -10,3 +10,10 @@ INSERT INTO users VALUES
     , ('gabewild37', '12301998')
     , ('gdude37', '43210')
     ;
+
+CREATE TABLE IF NOT EXISTS users_secure (
+    user VARCHAR(100) NOT NULL
+    , salt VARCHAR(100) NOT NULL
+    , pass VARCHAR(100) NOT NULL
+    , PRIMARY KEY(user)
+);
---
+++ phplogin/register.php
@@ -0,0 +1,56 @@
+<?php 
+
+function register() 
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "websys_login";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $pass = $_POST['pass'];
+    $user = $_POST['user'];
+
+    $hash_default_salt = password_hash($pass, PASSWORD_DEFAULT); 
+
+    $sql = "INSERT INTO users VALUES
+                (" + $user + "," + $hash_default_salt + ")";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+}
+
+if(isset($_POST['submit']))
+{
+    register();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+</head>
+<body>
+  <form method="post" action="register.php">
+    <label for="user">Username</label>
+    <input id="user" type="text">
+    <label for="pass">Password</label>
+    <input id="pass" type="password">
+    <input id="submit" type="submit">
+  </form>
+</body>
+</html>
