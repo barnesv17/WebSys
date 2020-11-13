8cffa0972b36d85bdada245982ab9d678bb22d50
gwild37 <gabewild37@gmail.com>
2020-11-13 13:10:02 -0500

    added html

--- phplogin/login_insecure.php
+++ phplogin/login_insecure.php
@@ -13,3 +13,19 @@ if ($conn->connect_error) {
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+</head>
+<body>
+  <form>
+    <label for="user">Username</label>
+    <input id="user" type="text">
+    <label for="pass">Password</label>
+    <input id="pass" type="password">
+  </form>
+</body>
+</html>
