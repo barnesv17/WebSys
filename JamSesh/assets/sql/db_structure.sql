4d2d577a02dbf4c9b5b64a687f71b0eb53105299
gwild37 <gabewild37@gmail.com>
2020-11-24 13:44:00 -0500

    added changes to db structure

--- JamSesh/assets/sql/db_structure.sql
+++ JamSesh/assets/sql/db_structure.sql
@@ -36,9 +36,21 @@ CREATE TABLE studioAdmins (
     , FOREIGN KEY(studioID) REFERENCES studios(studioID)
 );
 
+CREATE TABLE contributions (
+    username VARCHAR(20) NOT NULL 
+    , studioID INT NOT NULL
+    , instrument VARCHAR(20) NOT NULL 
+    , gitDiffPath VARCHAR(100) NOT NULL
+    , timesubmitted DATETIME NOT NULL 
+    , PRIMARY KEY (username, studioID, instrument, timesubmitted)
+    , FOREIGN KEY (username) REFERENCES users(username)
+    , FOREIGN KEY (studioID) REFERENCES studios(studioID)
+);
+
 INSERT INTO users VALUES
-    ("wildg@rpi.edu", NULL, "wildg");
-    , ("gabewild37@gmail.com", NULL, NULL);
+    ("wildg@rpi.edu", NULL, "wildg")
+    , ("gabewild37@gmail.com", NULL, NULL)
+    , ("gdude37@gmail.com", NULL, NULL);
 
 INSERT INTO studios VALUES
     (NULL, "My Studio", 1, "/repos/My-Studio");
@@ -51,4 +63,10 @@ INSERT INTO genres VALUES
     , (1, "Alternative");
 
 INSERT INTO studioAdmins VALUES
-    ("gabewild37@gmail.com", 1);
+    ("gabewild37@gmail.com", 1)
+    , ("gdude37@gmail.com", 1);
+
+-- Sample Queries
+SELECT s.studioName, g.genre FROM studios s, genres g WHERE s.studioID = g.studioID;
+SELECT s.studioName, so.username, sa.username FROM studios s, studioOwners so, studioAdmins sa WHERE s.studioID = so.studioID AND so.studioID = sa.studioID;
+INSERT INTO contributions VALUES("gdude37@gmail.com",1,"Lead Guitar","repos/diffs/My-Studio",CURRENT_TIMESTAMP);
