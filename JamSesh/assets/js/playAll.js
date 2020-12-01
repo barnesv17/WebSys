85d26c5868cf3f939813043880e13b7639c2410d
barnesv17 <virginiabarnes0825@gmail.com>
2020-11-30 20:56:11 -0500

    capitalized file names

--- JamSesh/assets/js/playAll.js
+++ JamSesh/assets/js/playAll.js
@@ -6,11 +6,3 @@ document.getElementById("play-all-btn").addEventListener( 'click', function() {
     (mp3s[i]).play();
   }
 });
-
-// function playAll() {
-//   var mp3s = document.getElementsByClassName( "mp3" );
-//   var mp3Length = mp3s.length;
-//   for( var i=0; i < mp3Length; i++ ) {
-//     alert( mp3s[i] );
-//   }
-// }
