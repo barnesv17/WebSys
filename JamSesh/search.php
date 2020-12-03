11ed4fc5d18c93591e70d263e6de6d4ceb810bfa
derekli2000 <derekli2011@gmail.com>
2020-12-03 16:49:51 -0500

    added rudimentary search page

--- JamSesh/search.php
+++ JamSesh/search.php
@@ -27,6 +27,23 @@
     </div>
   </nav>
 
-  </body>
+
+  <div class="mainBodyContainer">
+    <div class="searchBarContainer">
+      <div class="input-group md-form form-sm form-1 pl-0">
+        <div class="fa fa-search">
+          <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z" />
+            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+          </svg>
+        </div>
+        <div>
+          <input class="form-control my-0 py-1 searchBar" type="text" placeholder="Search" aria-label="Search">
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
 
 </html>
