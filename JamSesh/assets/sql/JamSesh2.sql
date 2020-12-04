2d288a027abaf2ab7296881358aa0d2caec3198f
gwild37 <gabewild37@gmail.com>
2020-12-04 00:45:14 -0500

    updated JamSesh2

--- JamSesh/assets/sql/JamSesh2.sql
+++ JamSesh/assets/sql/JamSesh2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2020 at 12:17 AM
+-- Generation Time: Dec 04, 2020 at 06:44 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -42,13 +42,34 @@ INSERT INTO `collaborators` (`studioID`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `favorites`
+--
+
+CREATE TABLE `favorites` (
+  `email` varchar(50) NOT NULL,
+  `studioID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `genres`
 --
 
 CREATE TABLE `genres` (
   `studioID` int(11) NOT NULL,
   `genre` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `genres`
+--
+
+INSERT INTO `genres` (`studioID`, `genre`) VALUES
+(2, 'Comedy'),
+(37, 'Children\'s'),
+(37, 'Classical'),
+(37, 'Electronic');
 
 -- --------------------------------------------------------
 
@@ -61,19 +82,22 @@ CREATE TABLE `studios` (
   `owner` varchar(50) DEFAULT NULL,
   `instruments` mediumtext DEFAULT '',
   `settings` mediumtext DEFAULT '',
-  `forks` int(10) NOT NULL DEFAULT 0
+  `forks` int(10) NOT NULL DEFAULT 0,
+  `isAFork` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `studios`
 --
 
-INSERT INTO `studios` (`id`, `owner`, `instruments`, `settings`, `forks`) VALUES
-(1, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Drums\",\"Viola\",\"Violin 1\",\"Violin 2\",\"Cello\"],\"files\":[\"studios/1/Drums.mp3\",\"studios/1/Viola.mp3\",\"studios/1/Violin 1.mp3\",\"studios/1/Violin 2.mp3\",\"studios/1/Cello.mp3\"]}', '{\"title\":\"Bad Guy!!!!\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"my attempt at being billy eilish\",\"genres\":[\"Jazz\"]}', 0),
-(2, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Cello\",\"Drums\",\"Viola\",\"Violin 1\",\"Violin 2\"],\"files\":[\"studios/1/Cello.mp3\",\"studios/1/Drums.mp3\",\"studios/1/Viola.mp3\",\"studios/1/Violin 1.mp3\",\"studios/1/Violin 2.mp3\"]}', '{\"title\":\"Bad Guy2\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"example description\",\"genres\":[]}', 0),
-(13, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Cello\"],\"files\":[\"studios/13/Cello.mp3\"]}', '{\"title\":\"Whatsup\",\"visibility\":\"Private\",\"allowFork\":\"No\",\"description\":\"This is a test!!!!\",\"genres\":[]}', 0),
-(14, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', '{ \"title\" : \"Take On Me\",\r\n                              \"visibility\" : \"Public\",\r\n                              \"allowFork\" : \"Yes\",\r\n                              \"description\" : \"a song by a-ha!\",\r\n                              \"genres\" : [] }', 0),
-(31, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Alto Sax\",\"Banjo\",\"Bass\",\"Cello\",\"Clarinet\",\"Drumset\",\"Euphonium\",\"Flute\",\"Honky Tonk Piano\",\"Horn\",\"Tenor Sax\",\"Trombone\",\"Trumpet 1\",\"Trumpet 2\",\"Tuba\",\"Viola\",\"Violin\"],\"files\":[\"studios/31/Alto Sax.mp3\",\"studios/31/Banjo.mp3\",\"studios/31/Bass.mp3\",\"studios/31/Cello.mp3\",\"studios/31/Clarinet.mp3\",\"studios/31/Drumset.mp3\",\"studios/31/Euphonium.mp3\",\"studios/31/Flute.mp3\",\"studios/31/Honky Tonk Piano.mp3\",\"studios/31/Horn.mp3\",\"studios/31/Tenor Sax.mp3\",\"studios/31/Trombone.mp3\",\"studios/31/Trumpet 1.mp3\",\"studios/31/Trumpet 2.mp3\",\"studios/31/Tuba.mp3\",\"studios/31/Viola.mp3\",\"studios/31/Violin.mp3\"]}', '{\"title\":\"Baby Shark Orchestra\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"do doo doo do do\",\"genres\":[]}', 0);
+INSERT INTO `studios` (`id`, `owner`, `instruments`, `settings`, `forks`, `isAFork`) VALUES
+(1, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Drums\",\"Viola\",\"Violin 1\",\"Violin 2\",\"Cello\"],\"files\":[\"studios/1/Drums.mp3\",\"studios/1/Viola.mp3\",\"studios/1/Violin 1.mp3\",\"studios/1/Violin 2.mp3\",\"studios/1/Cello.mp3\"]}', '{\"title\":\"Bad Guy!!!!\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"my attempt at being billy eilish\",\"genres\":[\"Jazz\"]}', 0, NULL),
+(2, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Cello\",\"Drums\",\"Viola\",\"Violin 1\",\"Violin 2\"],\"files\":[\"studios/1/Cello.mp3\",\"studios/1/Drums.mp3\",\"studios/1/Viola.mp3\",\"studios/1/Violin 1.mp3\",\"studios/1/Violin 2.mp3\"]}', '{\"title\":\"Bad Guy2\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"example description\",\"genres\":[]}', 0, NULL),
+(13, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Cello\"],\"files\":[\"studios/13/Cello.mp3\"]}', '{\"title\":\"Whatsup\",\"visibility\":\"Private\",\"allowFork\":\"No\",\"description\":\"This is a test!!!!\",\"genres\":[]}', 0, NULL),
+(14, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', '{ \"title\" : \"Take On Me\",\r\n                              \"visibility\" : \"Public\",\r\n                              \"allowFork\" : \"Yes\",\r\n                              \"description\" : \"a song by a-ha!\",\r\n                              \"genres\" : [] }', 0, NULL),
+(31, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Alto Sax\",\"Banjo\",\"Bass\",\"Cello\",\"Clarinet\",\"Drumset\",\"Euphonium\",\"Flute\",\"Honky Tonk Piano\",\"Horn\",\"Tenor Sax\",\"Trombone\",\"Trumpet 1\",\"Trumpet 2\",\"Tuba\",\"Viola\",\"Violin\"],\"files\":[\"studios/31/Alto Sax.mp3\",\"studios/31/Banjo.mp3\",\"studios/31/Bass.mp3\",\"studios/31/Cello.mp3\",\"studios/31/Clarinet.mp3\",\"studios/31/Drumset.mp3\",\"studios/31/Euphonium.mp3\",\"studios/31/Flute.mp3\",\"studios/31/Honky Tonk Piano.mp3\",\"studios/31/Horn.mp3\",\"studios/31/Tenor Sax.mp3\",\"studios/31/Trombone.mp3\",\"studios/31/Trumpet 1.mp3\",\"studios/31/Trumpet 2.mp3\",\"studios/31/Tuba.mp3\",\"studios/31/Viola.mp3\",\"studios/31/Violin.mp3\"]}', '{\"title\":\"Baby Shark Orchestra\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"do doo doo do do\",\"genres\":[]}', 0, NULL),
+(32, 'wildg@rpi.edu', '{ \"names\" : [], \"files\" : [] }', '{\"title\":\"My Studio\",\"visibility\":\"Public\",\"allowFork\":\"Yes\",\"description\":\"Description\",\"genres\":[]}', 0, NULL),
+(37, 'test@email.com', '{ \"names\" : [], \"files\" : [] }', '{\"title\":\"My Studio\",\"visibility\":\"Public\",\"allowFork\":\"No\",\"description\":\"Description\",\"genres\":[]}', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -82,7 +106,6 @@ INSERT INTO `studios` (`id`, `owner`, `instruments`, `settings`, `forks`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `username` varchar(255) DEFAULT 'random_username',
@@ -95,10 +118,13 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `username`, `displayName`, `bio`, `profilePic`) VALUES
-(1, 'virginiabarnes0825@gmail.com', '$2y$10$k2WzMzkOjGmZNaTC.Mw7l.swj..rbbefS5zVCYtZWnRgrwPx4uoIm', 'barnesv17', 'Virginia Barnes', 'this is my bio', 'assets/img/profile-pictures/profilepic3.jpg'),
-(2, 'barnev@rpi.edu', '$2y$10$dJ2rG/KxcJGZ5bhsRpjkWuwwF85wLz61GtFRmMmFua.qmCFyCrczK', 'johne', 'Elton John', '', 'assets/img/profile-pictures/profilepic1.jpg'),
-(4, 'test@gmail.com', '$2y$10$cMRFVJOuQrQ9U8ygAtTdS.lhB9hCKt6ZFFAFWbYwz6vWaEJGTXxca', 'random_username', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png');
+INSERT INTO `users` (`email`, `password`, `username`, `displayName`, `bio`, `profilePic`) VALUES
+('barnev@rpi.edu', '$2y$10$dJ2rG/KxcJGZ5bhsRpjkWuwwF85wLz61GtFRmMmFua.qmCFyCrczK', 'johne', 'Elton John', '', 'assets/img/profile-pictures/profilepic1.jpg'),
+('test@email.com', '$2y$10$.4b4av6PVS18iz.zMD1HcOk/Yr.HLLiI.1Ux5NiyqBVjijyZdqX1.', 'test3', 'My Name', 'kjhalkjhdflskjh', 'assets/img/profile-pictures/profilepic2.jpg'),
+('test@gmail.com', '$2y$10$cMRFVJOuQrQ9U8ygAtTdS.lhB9hCKt6ZFFAFWbYwz6vWaEJGTXxca', 'random_username', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png'),
+('test@mail.com', '$2y$10$f2zOx2whq9RzX/bC54M2aO5xQzP70tP8IILgwyK3NmbnoMgMyo.qG', 'test', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png'),
+('virginiabarnes0825@gmail.com', '$2y$10$k2WzMzkOjGmZNaTC.Mw7l.swj..rbbefS5zVCYtZWnRgrwPx4uoIm', 'barnesv17', 'Virginia Barnes', 'this is my bio', 'assets/img/profile-pictures/profilepic3.jpg'),
+('wildg@rpi.edu', '$2y$10$TnSWLoN7kq8KV6Ga3Zh6TeFyxCgCIP1WBPWE8THUBA.p6rYPfoSWq', 'wildg', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +138,13 @@ ALTER TABLE `collaborators`
   ADD KEY `email` (`email`);
 
 --
+-- Indexes for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD PRIMARY KEY (`email`,`studioID`),
+  ADD KEY `studioID` (`studioID`);
+
+--
 -- Indexes for table `genres`
 --
 ALTER TABLE `genres`
@@ -122,14 +155,14 @@ ALTER TABLE `genres`
 --
 ALTER TABLE `studios`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `owner` (`owner`);
+  ADD KEY `owner` (`owner`),
+  ADD KEY `isAFork` (`isAFork`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`) USING BTREE;
+  ADD PRIMARY KEY (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -139,13 +172,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `studios`
 --
 ALTER TABLE `studios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- Constraints for dumped tables
@@ -159,6 +186,13 @@ ALTER TABLE `collaborators`
   ADD CONSTRAINT `collaborators_ibfk_2` FOREIGN KEY (`studioID`) REFERENCES `studios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
+-- Constraints for table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`studioID`) REFERENCES `studios` (`id`);
+
+--
 -- Constraints for table `genres`
 --
 ALTER TABLE `genres`
@@ -168,7 +202,8 @@ ALTER TABLE `genres`
 -- Constraints for table `studios`
 --
 ALTER TABLE `studios`
-  ADD CONSTRAINT `studios_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `studios_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `users` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `studios_ibfk_2` FOREIGN KEY (`isAFork`) REFERENCES `studios` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
