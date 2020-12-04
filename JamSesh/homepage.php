8599221f7f08a9792c9f3ffaf92c416c14b7c173
derekli2000 <derekli2011@gmail.com>
2020-12-04 04:02:34 -0500

    fixed to fit new db schema

--- JamSesh/homepage.php
+++ JamSesh/homepage.php
@@ -1,139 +1,138 @@
 <?php
-  include 'assets/php/db_conn.php';
+  session_start();
 
-  // Define variables and initialize with empty values
-  $email = $password = $confirm_password = $username ="";
-  $email_err = $password_err = $confirm_password_err = "";
+include 'assets/php/db_conn.php';
 
-  if( $_SERVER["REQUEST_METHOD"] == "POST" ) {
+// Define variables and initialize with empty values
+$email = $password = $confirm_password = $username = "";
+$email_err = $password_err = $confirm_password_err = "";
 
-    // Validate email
-    if(empty(trim($_POST["email"]))) {
-      $email_err = "Please enter an email";
-      echo "<script>alert( 'Please enter an email' );</script>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  // Validate email
+  if (empty(trim($_POST["email"]))) {
+    $email_err = "Please enter an email";
+    echo "<script>alert( 'Please enter an email' );</script>";
+  } else {
+    // Prepare a select statement
+    $sql = "SELECT * FROM users WHERE email = ?";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "s", $param_email);
+      // Set parameters
+      $param_email = trim($_POST["email"]);
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        // Store result
+        mysqli_stmt_store_result($stmt);
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+          $email_err = "This email already has a registered account";
+          echo "<script>alert( 'This email already has a registered account' );</script>";
+        } else {
+          $email = trim($_POST["email"]);
+        }
+      } else {
+        echo "Oops! Something went wrong. Please try again later.";
+      }
+      // Close statement
+      mysqli_stmt_close($stmt);
     }
-    else {
-      // Prepare a select statement
-      $sql = "SELECT id FROM users WHERE email = ?";
-      if( $stmt = mysqli_prepare($link, $sql)) {
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param( $stmt, "s", $param_email );
-        // Set parameters
-        $param_email = trim( $_POST["email"] );
-        // Attempt to execute the prepared statement
-        if( mysqli_stmt_execute($stmt) ) {
-          // Store result
-          mysqli_stmt_store_result($stmt);
-          if( mysqli_stmt_num_rows($stmt) == 1 ) {
-            $email_err = "This email already has a registered account";
-            echo "<script>alert( 'This email already has a registered account' );</script>";
-          }
-          else {
-            $email = trim( $_POST["email"] );
+  }
+
+  // Validate username
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter a username";
+    echo "<script>alert( 'Please enter a username' );</script>";
+  } else {
+    $username = trim($_POST["username"]);
+  }
+
+  // Validate password
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter a password.";
+    echo "<script>alert( 'Please enter a password.' );</script>";
+  } elseif (strlen(trim($_POST["password"])) < 6) {
+    $password_err = "Password must have atleast 6 characters.";
+    echo "<script>alert( 'Password must have atleast 6 characters.' );</script>";
+  } else {
+    $password = trim($_POST["password"]);
+  }
+
+  // Validate confirm password
+  if (empty(trim($_POST["confirm_password"]))) {
+    $confirm_password_err = "Please confirm password.";
+    echo "<script>alert( 'Please confirm password.' );</script>";
+  } else {
+    $confirm_password = trim($_POST["confirm_password"]);
+    if (empty($password_err) && ($password != $confirm_password)) {
+      $confirm_password_err = "Password did not match.";
+      echo "<script>alert( 'Password did not match.' );</script>";
+    }
+  }
+
+  // Check input errors before inserting in database
+  if (empty($email_err) && empty($password_err) && empty($username_err) && empty($confirm_password_err)) {
+
+    // Prepare an insert statement
+    $sql = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "sss", $param_email, $param_password, $param_username);
+
+      // Set parameters
+      $param_email = $email;
+      $param_password = password_hash($password, PASSWORD_DEFAULT);
+      $param_username = $username;
+
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        // Redirect to user-profile page
+        header("location: user-profile.php");
+      } else {
+        echo "Something went wrong. Please try again later.";
+      }
+
+      // Close statement
+      mysqli_stmt_close($stmt);
+    }
+    // Direct to the user-profile page
+    // Prepare a select statement
+    $sql = "SELECT id, email, password, username, displayName, bio, profilePic FROM users WHERE email = ?";
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      // Bind variables to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "s", $param_email);
+      // Set parameters
+      $param_email = $email;
+      // Attempt to execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        // Store result
+        mysqli_stmt_store_result($stmt);
+        // Check if email exists, if yes then verify password
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+          // Bind result variables
+          mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password, $username, $displayName, $bio, $profilePic);
+          if (mysqli_stmt_fetch($stmt)) {
+            session_start();
+
+            // Store data in session variables
+            $_SESSION["loggedin"] = true;
+            $_SESSION["id"] = $id;
+            $_SESSION["email"] = $email;
+            $_SESSION["username"] = $username;
+            $_SESSION["displayName"] = $displayName;
+            $_SESSION["bio"] = $bio;
+            $_SESSION["profilePic"] = $profilePic;
+
+            // Redirect user to user profile page
+            header("location: user-profile.php");
           }
         }
-        else {
-          echo "Oops! Something went wrong. Please try again later.";
-        }
-        // Close statement
-        mysqli_stmt_close($stmt);
       }
     }
-
-    // Validate username
-    if(empty(trim($_POST["username"]))) {
-      $username_err = "Please enter a username";
-      echo "<script>alert( 'Please enter a username' );</script>";
-    }
-    else {
-      $username = trim($_POST["username"]);
-    }
-
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";
-        echo "<script>alert( 'Please enter a password.' );</script>";
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
-        echo "<script>alert( 'Password must have atleast 6 characters.' );</script>";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";
-        echo "<script>alert( 'Please confirm password.' );</script>";
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-            echo "<script>alert( 'Password did not match.' );</script>";
-        }
-    }
-
-    // Check input errors before inserting in database
-    if(empty($email_err) && empty($password_err) && empty($username_err) && empty($confirm_password_err)){
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_email, $param_password, $param_username);
-
-            // Set parameters
-            $param_email = $email;
-            $param_password = password_hash( $password, PASSWORD_DEFAULT );
-            $param_username = $username;
-
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to user-profile page
-                header("location: user-profile.php");
-            } else {
-                echo "Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-        // Direct to the user-profile page
-        // Prepare a select statement
-        $sql = "SELECT id, email, password, username, displayName, bio, profilePic FROM users WHERE email = ?";
-        if($stmt = mysqli_prepare($link, $sql)){
-          // Bind variables to the prepared statement as parameters
-          mysqli_stmt_bind_param($stmt, "s", $param_email);
-          // Set parameters
-          $param_email = $email;
-          // Attempt to execute the prepared statement
-          if(mysqli_stmt_execute($stmt)){
-            // Store result
-            mysqli_stmt_store_result($stmt);
-            // Check if email exists, if yes then verify password
-            if(mysqli_stmt_num_rows($stmt) == 1){
-              // Bind result variables
-              mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password, $username, $displayName, $bio, $profilePic);
-              if(mysqli_stmt_fetch($stmt)){
-                session_start();
-
-                // Store data in session variables
-                $_SESSION["loggedin"] = true;
-                $_SESSION["id"] = $id;
-                $_SESSION["email"] = $email;
-                $_SESSION["username"] = $username;
-                $_SESSION["displayName"] = $displayName;
-                $_SESSION["bio"] = $bio;
-                $_SESSION["profilePic"] = $profilePic;
-
-                // Redirect user to user profile page
-                header("location: user-profile.php");
-              }
-            }
-          }
-        }
-    }
-    // Close connection
-    mysqli_close($link);
   }
+  // Close connection
+  mysqli_close($link);
+}
 ?>
 
 <!DOCTYPE html>
