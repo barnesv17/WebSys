057171c4fc406d2f2d51e4485b03553aa5755b54
Kyle Qin <kylezehaoqin@users.noreply.github.com>
2020-12-11 20:49:54 -0500

    added favorites column in studio table

--- JamSesh/assets/sql/JamSesh2.sql
+++ JamSesh/assets/sql/JamSesh2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2020 at 10:46 AM
+-- Generation Time: Dec 12, 2020 at 02:48 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -37,7 +37,8 @@ CREATE TABLE `collaborators` (
 --
 
 INSERT INTO `collaborators` (`studioID`, `email`) VALUES
-(14, 'barnev@rpi.edu');
+(14, 'barnev@rpi.edu'),
+(51, 'root@rpi.edu');
 
 -- --------------------------------------------------------
 
@@ -49,6 +50,14 @@ CREATE TABLE `favorites` (
   `email` varchar(50) NOT NULL,
   `studioID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `favorites`
+--
+
+INSERT INTO `favorites` (`email`, `studioID`) VALUES
+('root@rpi.edu', 52),
+('root@rpi.edu', 57);
 
 -- --------------------------------------------------------
 
@@ -85,25 +94,31 @@ CREATE TABLE `studios` (
   `allowFork` varchar(5) NOT NULL DEFAULT 'Yes',
   `description` varchar(2048) NOT NULL DEFAULT 'Description',
   `forks` int(10) NOT NULL DEFAULT 0,
-  `isAFork` int(11) DEFAULT NULL
+  `isAFork` int(11) DEFAULT NULL,
+  `favorites` int(255) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `studios`
 --
 
-INSERT INTO `studios` (`id`, `owner`, `instruments`, `title`, `visibility`, `allowFork`, `description`, `forks`, `isAFork`) VALUES
-(14, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 1, NULL),
-(31, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Alto Sax\",\"Banjo\",\"Bass\",\"Cello\",\"Clarinet\",\"Drumset\",\"Euphonium\",\"Flute\",\"Honky Tonk Piano\",\"Horn\",\"Tenor Sax\",\"Trombone\",\"Trumpet 1\",\"Trumpet 2\",\"Tuba\",\"Viola\",\"Violin\"],\"files\":[\"studios/31/Alto Sax.mp3\",\"studios/31/Banjo.mp3\",\"studios/31/Bass.mp3\",\"studios/31/Cello.mp3\",\"studios/31/Clarinet.mp3\",\"studios/31/Drumset.mp3\",\"studios/31/Euphonium.mp3\",\"studios/31/Flute.mp3\",\"studios/31/Honky Tonk Piano.mp3\",\"studios/31/Horn.mp3\",\"studios/31/Tenor Sax.mp3\",\"studios/31/Trombone.mp3\",\"studios/31/Trumpet 1.mp3\",\"studios/31/Trumpet 2.mp3\",\"studios/31/Tuba.mp3\",\"studios/31/Viola.mp3\",\"studios/31/Violin.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(40, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(41, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(42, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(43, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(45, 'virginiabarnes0825@gmail.com', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(48, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL),
-(50, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Hanukkah Party', 'Public', 'Yes', 'We love Hanukkah', 0, NULL),
-(51, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Cities on Flame with Jam and Sesh', 'Public', 'Yes', 'ft. Blue Oyster Heathens', 1, NULL),
-(52, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Blade Runner', 'Public', 'Yes', 'ft. CyberBUNk', 0, NULL);
+INSERT INTO `studios` (`id`, `owner`, `instruments`, `title`, `visibility`, `allowFork`, `description`, `forks`, `isAFork`, `favorites`) VALUES
+(14, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 3, NULL, 0),
+(31, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Alto Sax\",\"Banjo\",\"Bass\",\"Cello\",\"Clarinet\",\"Drumset\",\"Euphonium\",\"Flute\",\"Honky Tonk Piano\",\"Horn\",\"Tenor Sax\",\"Trombone\",\"Trumpet 1\",\"Trumpet 2\",\"Tuba\",\"Viola\",\"Violin\"],\"files\":[\"studios/31/Alto Sax.mp3\",\"studios/31/Banjo.mp3\",\"studios/31/Bass.mp3\",\"studios/31/Cello.mp3\",\"studios/31/Clarinet.mp3\",\"studios/31/Drumset.mp3\",\"studios/31/Euphonium.mp3\",\"studios/31/Flute.mp3\",\"studios/31/Honky Tonk Piano.mp3\",\"studios/31/Horn.mp3\",\"studios/31/Tenor Sax.mp3\",\"studios/31/Trombone.mp3\",\"studios/31/Trumpet 1.mp3\",\"studios/31/Trumpet 2.mp3\",\"studios/31/Tuba.mp3\",\"studios/31/Viola.mp3\",\"studios/31/Violin.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(40, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(41, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(42, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(43, 'barnev@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(45, 'virginiabarnes0825@gmail.com', '{ \"names\" : [], \"files\" : [] }', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(48, 'virginiabarnes0825@gmail.com', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(50, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Hanukkah Party', 'Public', 'Yes', 'We love Hanukkah', 0, NULL, 1),
+(51, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Cities on Flame with Jam and Sesh', 'Public', 'Yes', 'ft. Blue Oyster Heathens', 2, NULL, 0),
+(52, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Blade Runner', 'Public', 'Yes', 'ft. CyberBUNk', 0, NULL, 1),
+(53, 'qinz@rpi.edu', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(54, 'root@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Cities on Flame with Jam and Sesh', 'Public', 'Yes', 'ft. Blue Oyster Heathens', 0, NULL, 0),
+(55, 'qinz@rpi.edu', '{\"names\":[\"Oboe\",\"Cello\",\"Piano\",\"Guitar 1\",\"Guitar 2\"],\"files\":[\"studios/14/Oboe.mp3\",\"studios/14/Cello.mp3\",\"studios/14/Piano.mp3\",\"studios/14/Guitar 1.mp3\",\"studios/14/Guitar 2.mp3\"]}', 'Title', 'Public', 'Yes', 'Description', 0, NULL, 0),
+(56, 'root@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Hanukkah Party', 'Public', 'Yes', 'We love Hanukkah', 0, NULL, 0),
+(57, 'qinz@rpi.edu', '{ \"names\" : [], \"files\" : [] }', 'Enderman is alive', 'Public', 'Yes', 'ft. Hercraft', 0, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -128,7 +143,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`email`, `password`, `username`, `displayName`, `bio`, `profilePic`, `isAdmin`) VALUES
 ('barnev@rpi.edu', '$2y$10$dJ2rG/KxcJGZ5bhsRpjkWuwwF85wLz61GtFRmMmFua.qmCFyCrczK', 'johne', 'Elton John', '', 'assets/img/profile-pictures/profilepic1.jpg', 'Yes'),
 ('qinz@rpi.edu', '$2y$10$2ksj0hI1a2Nm6r0MOOmX2OIGl2aEsZ/mPzKl9/bzbHM/t8bhRt3Dy', 'kylecccx', 'Kyle Qin', 'I used Nigua Hamooz on my burger king order yesterday', 'assets/img/profile-pictures/profilepic1.jpg', 'Yes'),
-('root@rpi.edu', '$2y$10$YTvU9YTUYao7FNb/yf6H2OvDmNfWntWsCeaw8bLq/S1y8CnAfLWca', 'root_test', 'root_test', '', 'assets/img/profile-pictures/blank-avatar.png', 'No'),
+('root@rpi.edu', '$2y$10$u9I/eSTRGa0pN1m6kPLvlOwylGQRKHh2Bz8pSfujUt0FFBLg0uauG', 'root_test', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png', 'No'),
 ('virginiabarnes0825@gmail.com', '$2y$10$k2WzMzkOjGmZNaTC.Mw7l.swj..rbbefS5zVCYtZWnRgrwPx4uoIm', 'barnesv', 'John Legend', 'hello', 'assets/img/profile-pictures/profilepic3.jpg', 'Yes'),
 ('wildg@rpi.edu', '$2y$10$TnSWLoN7kq8KV6Ga3Zh6TeFyxCgCIP1WBPWE8THUBA.p6rYPfoSWq', 'wildg', 'FirstName LastName', '', 'assets/img/profile-pictures/blank-avatar.png', 'Yes');
 
@@ -178,7 +193,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `studios`
 --
 ALTER TABLE `studios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- Constraints for dumped tables
